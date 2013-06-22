9aded62da954e06493d609801404007d28981e3a
James Cook <mokus@deepbondi.net>
2013-06-22 11:30:32 -0700

    re-inverted, more playing with main

--- src/lights.c
+++ src/lights.c
@@ -8,42 +8,42 @@
 #define setPin(X, pin, mode)    setBit(PORT ## X, pin, mode)
 
 void init_light_subsystem() {
-    pinMode(B, 2, 1);
-    pinMode(B, 3, 1);
-    pinMode(B, 4, 1);
-    pinMode(D, 5, 1);
-    
-    // timer 0: "fast PWM", output set at OCR0x/cleared at TOP, clock/64 (977 Hz)
-    TCCR0A = 0xF3;
+    // timer 0: "fast PWM", output set at TOP/cleared at OCR0x, clock/64 (977 Hz)
+    TCCR0A = 0xA3;
     TCCR0B = 0x03;
     
-    // timer 1: "fast PWM, 8 bit", output set at OCR1x/cleared at TOP, clock/64 (977 Hz)
-    TCCR1A = 0xF1;
+    // timer 1: "fast PWM, 8 bit", output set at TOP/cleared at OCR1x, clock/64 (977 Hz)
+    TCCR1A = 0xA1;
     TCCR1B = 0x0B;
     
-    OCR0A = 0x00;
-    OCR0B = 0x00;
-    OCR1A = 0x0000;
-    OCR1B = 0x0000;
+    set_channel_value(0, 0);
+    set_channel_value(1, 0);
+    set_channel_value(2, 0);
+    set_channel_value(3, 0);
 }
 
 void set_channel_value(uint8_t chan, uint8_t value) {
+    uint8_t mode = value ? 1 : 0;
     switch (chan) {
         default:
         case 0:
             OCR1A = value;
+            pinMode(B, 3, mode);
             break;
         
         case 1:
             OCR1B = value;
+            pinMode(B, 4, mode);
             break;
         
         case 2:
             OCR0A = value;
+            pinMode(B, 2, mode);
             break;
         
         case 3:
             OCR0B = value;
+            pinMode(D, 5, mode);
             break;
     }
     
--- src/main.c
+++ src/main.c
@@ -3,31 +3,20 @@
 
 #include "lights.h"
 
-void set_all(uint8_t i) {
-    set_channel_value(0, i);
-    set_channel_value(1, i);
-    set_channel_value(2, i);
-    set_channel_value(3, i);
+void set_rgb(uint8_t r, uint8_t g, uint8_t b) {
+    set_channel_value(0, g);
+    set_channel_value(1, b);
+    set_channel_value(2, r);
 }
 
 int main(void)
 {
     init_light_subsystem();
-
+    
+    uint8_t x = 0;
     while(1) {
-        // ramp up once
-        uint16_t i;
-        for (i = 0; i < 0x100; i++) {
-            _delay_ms(4);
-            set_all(i);
-        }
-
-        // blink a few times
-        for (i = 0; i < 10; i++) {
-            _delay_ms(50);
-            set_all(0x40);
-            _delay_ms(50);
-            set_all(0x7f);
-        }
+        set_rgb(x, x, x);
+        x++;
+        _delay_ms(10);
     }
 }
