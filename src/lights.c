20c5ba1ca2fd9755bb6e7cbf35fa1945f9c8fa68
James Cook <mokus@deepbondi.net>
2013-06-22 12:26:23 -0700

    switch to "phase-correct" pwm

--- src/lights.c
+++ src/lights.c
@@ -8,42 +8,43 @@
 #define setPin(X, pin, mode)    setBit(PORT ## X, pin, mode)
 
 void init_light_subsystem() {
-    // timer 0: "fast PWM", output set at TOP/cleared at OCR0x, clock/64 (977 Hz)
-    TCCR0A = 0xA3;
+    
+    // timer 0: "phase-correct PWM", output set at TOP/cleared at OCR0x, clock/64 (977 Hz)
+    TCCR0A = 0xA1;
     TCCR0B = 0x03;
     
-    // timer 1: "fast PWM, 8 bit", output set at TOP/cleared at OCR1x, clock/64 (977 Hz)
+    // timer 1: "phase-correct PWM, 8 bit", output set at TOP/cleared at OCR1x, clock/64 (977 Hz)
     TCCR1A = 0xA1;
-    TCCR1B = 0x0B;
+    TCCR1B = 0x03;
     
     set_channel_value(0, 0);
     set_channel_value(1, 0);
     set_channel_value(2, 0);
     set_channel_value(3, 0);
+    
+    pinMode(B, 2, 1);
+    pinMode(B, 3, 1);
+    pinMode(B, 4, 1);
+    pinMode(D, 5, 1);
 }
 
 void set_channel_value(uint8_t chan, uint8_t value) {
-    uint8_t mode = value ? 1 : 0;
     switch (chan) {
         default:
         case 0:
             OCR1A = value;
-            pinMode(B, 3, mode);
             break;
         
         case 1:
             OCR1B = value;
-            pinMode(B, 4, mode);
             break;
         
         case 2:
             OCR0A = value;
-            pinMode(B, 2, mode);
             break;
         
         case 3:
             OCR0B = value;
-            pinMode(D, 5, mode);
             break;
     }
     
