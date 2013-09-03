ce494863eafe8d00748f515ec86cedff17d7fe26
James Cook <mokus@deepbondi.net>
2013-09-02 22:19:03 -0700

    comments in linearize.c

--- src/linearize.c
+++ src/linearize.c
@@ -20,17 +20,26 @@ void linearize(
     uint16_t ch1 = *ch1_loc;
     uint16_t ch2 = *ch2_loc;
     
+    // sort channels into non-decreasing order of brightness
     if (ch0 > ch1) {swap(uint16_t,ch0,ch1); swap(volatile uint16_t *,ch0_loc,ch1_loc)}
     if (ch1 > ch2) {swap(uint16_t,ch1,ch2); swap(volatile uint16_t *,ch1_loc,ch2_loc)}
     if (ch0 > ch1) {swap(uint16_t,ch0,ch1); swap(volatile uint16_t *,ch0_loc,ch1_loc)}
     
-    if (ch2 > 0) {
+    // if ch0 == ch2, all channels are equal and no correction is needed.
+    // note that this accounts for the case where ch2 == 0, so the division
+    // below will always succeed.
+    if (ch0 < ch2) {
+        // channel weights corrected so that time*current is allocated in
+        // the proportion specified by ch0, ch1 and ch2
         uint32_t a = 3 * (uint32_t)ch0;
         uint32_t b = (uint32_t)ch0 + 2 * (uint32_t)ch1;
         uint32_t c = (uint32_t) ch0 + (uint32_t) ch1 + (uint32_t) ch2;
         
+        // normalization factor to map c to max intensity (i.e., ch2)
         uint16_t x = (uint16_t) (((uint32_t) ch2 << 16) / c);
         
+        // output the final corrected values
+        // (only ch0 and ch1, ch2 is already the desired value)
         *ch0_loc = (uint16_t) (a * (uint32_t) x >> 16);
         *ch1_loc = (uint16_t) (b * (uint32_t) x >> 16);
     }
