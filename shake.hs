9a0e13418dbef20f8ad453df782dcc0b72f9708d
James Cook <mokus@deepbondi.net>
2014-06-04 22:00:15 -0700

    cleaning up build script a bit

--- shake.hs
+++ shake.hs
@@ -1,7 +1,8 @@
 #!/usr/bin/env runhaskell
+{-# OPTIONS_GHC -Wall -fno-warn-missing-signatures #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Control.Monad
 import Development.Shake
 import Development.Shake.AVR
 import Development.Shake.FilePath
@@ -15,60 +16,67 @@ usbPort         = "usb"
 
 cFlags board = ["-Wall", "-Os", "-std=c99",
     "-Iinclude",
-    "-DF_CPU=" ++ show (boardClock board) ++ "UL",
+    "-DF_CPU=" ++ show (round (boardClock board) :: Integer) ++ "UL",
     "-mmcu=" ++ boardDevice board]
 
 data Board = Board
     { boardName     :: String
     , boardDevice   :: String
-    , boardClock    :: Integer
+    , boardClock    :: Rational
     }
 
-boards =
-    [ Board "teensy" "atmega32u4" (round 16e6)
-    , Board "xmega"  "atxmega8e5" (round 32e6)
-    ]
+replaceDirAndExt dir ext = flip replaceDirectory dir . flip replaceExtension ext
 
-main = shakeArgs shakeOptions $ do
-    want =<< mapM boardRules boards
-    
-    phony "clean" $ do
-        removeFilesAfter "." ["*.hex", "*.elf"]
-        sequence_
-            [ removeFilesAfter (buildRoot </> boardName board) ["*.o"]
-            | board <- boards
-            ]
-    
-    phony "flash" $ do -- only works for xmega
-        avrdude "flash" avrdudeFlags "vTree-xmega.hex" usbPort
-    
-    "*.hex" *> \out -> do
-        let elf = out `replaceExtension` "elf"
-        avr_objcopy "ihex" ["-j", ".text", "-j", ".data"] elf out
-
-boardRules board = do
-    let projBase = proj ++ "-" ++ boardName board
+-- rules for a single board:
+-- compile common sources from src/*.c and board
+-- sources from src/board/<board>/*.c
+boardRules board@Board{..} = do
+    -- board-specific file names and paths
+    let projBase = proj ++ "-" ++ boardName
         
-        boardDir        = srcDir </> "board" </> boardName board
-        buildDir        = buildRoot </> boardName board
-        boardBuildDir   = buildDir </> "board"
+        boardDir        = srcDir </> "board" </> boardName
+        buildDir        = buildRoot </> boardName
         
         hex = projBase <.> "hex"
-        elf = projBase <.> "elf"
+        elf = buildRoot </> projBase <.> "elf"
     
+    want [hex]
+    
+    -- to build the hex file, first build the elf an then objcopy it.
+    -- run avr_size on the elf, too, because I like to see where
+    -- things stand.
+    hex *> \out -> do
+        avr_objcopy "ihex" ["-j", ".text", "-j", ".data"] elf out
+    
+    -- to build the elf file, build an object file for every source file
+    -- in src/ and src/board/<board>/, then link those.
     elf *> \out -> do
         srcs        <- getDirectoryFiles srcDir   ["*.c"]
         boardSrcs   <- getDirectoryFiles boardDir ["*.c"]
-        let objs = [src `replaceDirectory` buildDir      `replaceExtension` "o" | src <- srcs]
-                ++ [src `replaceDirectory` boardBuildDir `replaceExtension` "o" | src <- boardSrcs]
+        let objs = map (replaceDirAndExt buildDir "o") (srcs ++ boardSrcs)
         avr_ld' "avr-gcc" (cFlags board) objs out
     
+    -- to build an object file, find the source (searching src/ first,
+    -- then src/board/<board>/) and compile it.
     buildDir </> "*.o" *> \out -> do
-        let input = out `replaceDirectory` srcDir `replaceExtension` "c"
-        avr_gcc (cFlags board) input out
-    
-    boardBuildDir </> "*.o" *> \out -> do
-        let input = out `replaceDirectory` boardDir `replaceExtension` "c"
+        let commonSrc = replaceDirAndExt srcDir   "c" out
+            boardSrc  = replaceDirAndExt boardDir "c" out
+        
+        isCommon <- doesFileExist commonSrc
+        let input = if isCommon then commonSrc else boardSrc
+        
         avr_gcc (cFlags board) input out
     
     return hex
+
+main = shakeArgs shakeOptions $ do
+    "clean" ~> removeFilesAfter buildRoot ["//*"]
+    
+    -- note that "flash" only works for xmega
+    -- (the teensy protocol isn't supported by avrdude)
+    "flash" ~> avrdude "flash" avrdudeFlags "vTree-xmega.hex" usbPort
+    
+    mapM_ boardRules
+        [ Board "teensy" "atmega32u4" 16e6
+        , Board "xmega"  "atxmega8e5" 32e6
+        ]
