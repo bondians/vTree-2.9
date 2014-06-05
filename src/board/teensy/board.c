629b2472768147acd451eb586ea23ecba66313d3
James Cook <mokus@deepbondi.net>
2014-06-04 22:04:36 -0700

    implemented UART setup for teensy, haven't tested yet though.

--- src/board/teensy/board.c
+++ src/board/teensy/board.c
@@ -1,6 +1,7 @@
 #include "board.h"
 
 #include "ir.h"
+#include "xbee.h"
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -17,15 +18,48 @@ ISR(TIMER0_COMPA_vect) {
     receive_ir_data(irdata);
 }
 
-void setup_clock() {
-    cli();
-    CLKPR = 0b10000000;
-    CLKPR = 0b00000000;
+#define BAUD 9600
+
+ISR(USART1_RX_vect) {
+    // read status and byte immediately
+    uint8_t status  = UCSR1A;
+    uint8_t in_byte = UDR1;
+    
+    if (status & (1 << FE1)) {
+        // reset parser on frame errors
+        xbee_reset_parser();
+    } else {
+        xbee_byte_received(in_byte);
+    }
+}
+
+// teensy starts up in CLKDIV8 mode (2 MHz).
+// all we need to do here is switch out of it.
+static void setup_clock() {
+    CLKPR = 0b10000000; // enable changing the clock prescaler
+    CLKPR = 0b00000000; // execute the change
+}
+
+// set up input to read IR sensor on PB4
+// and Timer0 to give an interrupt every 50 usec (800 cycles @ 16MHz)
+static void setup_ir_pin() {
+    DDRB &= ~(1 << DDB4);
+    
+    OCR0A = 99;
+    
+    // COM0A/B      = 00 (no PWM)
+    // WGM0         = 010 (CTC mode, TOP = OCR0A)
+    // CS           = 010 (1:8 with IO clock)
+    TCCR0A = 0b00000010;
+    TCCR0B = 0b00000010;
+    
+    // enable compare-match interrupt
+    TIMSK0 = 1 << OCIE0A;
 }
 
 // set up the light channels:
 // PORTB pins 5/6/7 (OCR1A/B/C)
-void setup_light_pins() {
+static void setup_light_pins() {
     ICR1 = 0xFFFF;
     OCR1A = OCR1B = OCR1C = 0;
     
@@ -40,27 +74,31 @@ void setup_light_pins() {
           | (1 << DDB7);
 }
 
-// set up input to read IR sensor on PB4
-// and Timer0 to give an interrupt every 50 usec (800 cycles @ 16MHz)
-void setup_ir_pin() {
-    DDRB &= ~(1 << DDB4);
+// initialize USART1 to the selected baud rate.
+// RX on PD2, TX on PD3.
+static void setup_xbee_uart() {
+    DDRD &= ~(1 << DDD2); // set RX as input
+    // DDRD |=   1 << DDD3; // set TX as output
     
-    OCR0A = 99;
+    // set baud rate
+    #include <util/setbaud.h>
+    UBRR1H = UBRRH_VALUE;
+    UBRR1L = UBRRL_VALUE;
+    UCSR1A = ((USE_2X ? 1 : 0) << U2X1);
     
-    // COM0A/B      = 00 (no PWM)
-    // WGM0         = 010 (CTC mode, TOP = OCR0A)
-    // CS           = 010 (1:8 with IO clock)
-    TCCR0A = 0b00000010;
-    TCCR0B = 0b00000010;
+    // set format to 8N1
+    UCSR1C = ((3<<UCSZ10) | (0<<UPM10) | (0<<USBS1));
     
-    TIMSK0 = 1 << OCIE0A;
+    // enable RX and RX-complete interrupt
+    UCSR1B |= (1 << RXCIE1) | (1 << RXEN1);
 }
 
 void init_board(void) {
     setup_clock();
     
-    setup_light_pins();
     setup_ir_pin();
+    setup_light_pins();
+    setup_xbee_uart();
     
     DDRD |= 1 << DDD6; // enable onboard LED output so we can blink it suggestively
     
